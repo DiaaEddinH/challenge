f7433cd692691b09d0034e3f0ee8ddedcaf8056e
DiaaEddinH <diaaeddinh@gmail.com>
2024-04-17 10:58:54 +0100

    Debug survey data

--- bin/run_fractal.sh
+++ bin/run_fractal.sh
@@ -1,5 +1,5 @@
 mkdir -p data
-curl -L -o data/surveys.csv https://energydata.info/dataset/a27a9b60-706b-4c81-8608-c913d2ed998f/resource/fdef4f22-fe57-49b1-9c42-e5dac79cc90c/download/pakistanbiomassfieldsurvey.csv
+curl -L -o data/survey.csv https://energydata.info/dataset/a27a9b60-706b-4c81-8608-c913d2ed998f/resource/fdef4f22-fe57-49b1-9c42-e5dac79cc90c/download/pakistanbiomassfieldsurvey.csv
 
 mkdir -p results
 
